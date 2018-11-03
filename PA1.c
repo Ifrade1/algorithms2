f1e786f46bfe7d251ed1298c2b37d4e3c257e6eb
Isabel Frade <33909806+Ifrade1@users.noreply.github.com>
2018-11-03 11:51:48 -0400

    Update PA1.c

--- PA1.c
+++ PA1.c
@@ -1,6 +1,22 @@
 //This is the main file.
 #include <stdio.h>
+#include<string.h>
+
 int main() {
      int n; //this is the number that has to be saved into the tree
+     char str1[50];
+     while(scanf("%s",str1, "%d", &n) !=EOF){ //scans until end of file
+            /*if str1 = INS
+            else if str1 == DEL
+            else if str1 == SEA
+            else if str1 == MIN
+            else if str1 == MAX
+            else if str1 == PRE
+            else if str1 == SUC
+            else if str1 == HEI
+            else { if str == anything else, spit out error
+                */
+    return 0;
+    }
 }
 
