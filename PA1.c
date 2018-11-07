c6bd6d2758d973d39294b52f9c5b8b4ee25103d0
Isabel Frade <33909806+Ifrade1@users.noreply.github.com>
2018-11-06 21:55:41 -0500

    Update PA1.c

--- PA1.c
+++ PA1.c
@@ -8,14 +8,37 @@ struct Tree
     struct Tree *left_child, *right_child,*ide_val, *parent; //ide_val is pointer list to values identical to it
 };
 //insert
+struct Tree *search(struct Tree *leaf, int item) {
+    if (leaf == NULL) {
+        return NULL;
+    }
+    else if (leaf->val == item) {
+        return leaf;
+    }
+    else if (leaf->val > item) {
+        return search(leaf->left_child, item);
+    }
+    else if (leaf->val > item){
+    return search(leaf->right_child, item);
+    }
+}
 struct Tree *newLeaf(int x){
-	struct Tree *tempy =  malloc(sizeof(struct Tree));
-    	tempy->val = x;
-    	tempy->left_child = NULL;
-    	tempy->right_child = NULL;
-    	tempy->ide_val = NULL;
-    	tempy->parent = NULL;
+        struct Tree *tempy =  malloc(sizeof(struct Tree));
+    tempy->val = x;
+    tempy->left_child = NULL;
+    tempy->right_child = NULL;
+    tempy->ide_val = NULL;
+    tempy->parent = NULL;
     return tempy;
+}
+int height(struct Tree* root){
+    if (root == NULL) return -1;
+    else {
+        int Lheight = height(root->left_child);
+        int Rheight = height(root->right_child);
+        if (Lheight > Rheight) return(Lheight+1);
+        else return(Rheight+1);
+    }
 }
 struct Tree* insert(struct Tree *tree, int item){
     struct Tree *lchild = tree;
@@ -46,83 +69,69 @@ else if (item < tree->val)
 return tree;
 }
 //delete
-struct Tree *deletey(struct Tree *tree, int item){
+int deletey(struct Tree *tree, int item){
        struct Tree *temp = tree;
        struct Tree *parent = tree;
-        if (tree == NULL) return NULL;
-       if(item < tree->val){
-		tree->left_child = deletey(tree->left_child, item);//key is in the left subtree
-	}
+       struct Tree *min = tree;//get smallest node in right subtree
+       int del_num;
+        if (tree == NULL) return 0;
+        if(item < tree->val){
+            tree = search(tree->left_child, item);//key is in the left subtree
+        }
 	else if(item > tree->val){
-		tree->right_child = deletey(tree->right_child, item);//key is in right subtree
-	}
-    if (item == tree->val){//nodes have the same key Is this supposed to be an else statement?
+		tree= search(tree->right_child, item);//key is in right subtree
+        }
+    if (item == tree->val){//nodes have the same key
+        del_num = 1;
         parent = tree->parent;
 	    while (temp->ide_val != NULL){
            tree = temp;
             temp= temp->ide_val;
             free(tree);
+            del_num = del_num++;
 	    }
 	    if((tree->left_child)== NULL && (tree->right_child == NULL)){
 	         if ((parent->left_child)->val == (tree)->val){
                 parent->left_child = NULL;
                 free(tree);
-                return parent->left_child;
                 }
             else if ((parent->right_child)->val == (tree)->val){
                 parent->right_child = NULL;
                 free(tree);
-                return parent->right_child;
             }
 	    }
 		if((tree->left_child)== NULL && (tree->right_child != NULL)){//node has 0 children or 1 child
             if ((parent->left_child)->val == (tree)->val){
                 parent->left_child = tree->right_child;
                 free(tree);
-                 return parent->left_child;
                 }
             else if ((parent->right_child)->val == (tree)->val){
                 parent->right_child = tree->right_child;
                 free(tree);
-                 return parent->right_child;
             }
 		}
-		else if((tree->right_child == NULL) && (tree->left_child != NULL)){
+		if((tree->right_child == NULL) && (tree->left_child != NULL)){
             if ((parent->left_child)->val == (tree)->val){
                 parent->left_child = tree->left_child;
                 free(tree);
-                return parent->left_child;
                 }
             else if ((parent->right_child)->val == (tree)->val){
                 parent->right_child = tree->left_child;
                 free(tree);
-                 return parent->right_child;
             }
-
 		}
-		else if((tree->right_child != NULL) && (tree->left_child != NULL)){
-		struct Tree *min = tree;//get smallest node in right subtree
-            if(min == NULL) { return min;
-            }
+		else if(tree->right_child != NULL && tree->left_child != NULL){
+            min = tree;
 		while(min->right_child != NULL){
 			min = min->right_child;
 		}
 		tree->val = min->val;//copies contents to current node
-		tree->right_child = deletey(tree->right_child, min->val);//deletes node
+		(min->parent)->right_child = NULL;
+		free(min);
 		}
+		}
+		 return del_num;
 	}
-	return tree;//end of delete
-	}//end of delete
-
-int height(struct Tree* root){
-    if (root == NULL) return -1;
-    else {
-        int Lheight = height(root->left_child);
-        int Rheight = height(root->right_child);
-        if (Lheight > Rheight) return(Lheight+1);
-        else return(Rheight+1);
-    }
-} //end of height
 
  struct Tree *minimum(struct Tree *tree){
     struct Tree *min = tree;
@@ -141,21 +150,10 @@ int height(struct Tree* root){
     return (max->val);
 }
 
-struct Tree * search(struct Tree * leaf, int item) {
-    if (leaf == NULL) {
-        return NULL;
-    }
-    else if (leaf->val == item) {
-        return leaf;
-    }
-    else if (leaf->val > item) {
-        return search(leaf->left_child, item);
-    }
-    return search(leaf->right_child, item);
-}
+
 
 //successor function
-struct Tree * succ(struct Tree *root, struct Tree *succ){//segmentation fault after executing command
+struct Tree * succ(struct Tree *root, struct Tree *succ){
 	if(succ->right_child != NULL)//successor is the node with the minimum key value in right subtree
 		return minimum(succ->right_child);
 	struct Tree *temp= succ->ide_val;
@@ -168,13 +166,14 @@ struct Tree * succ(struct Tree *root, struct Tree *succ){//segmentation fault af
 
 //predecessor function
 struct Tree * pred(struct Tree *tree, int item){
-	struct Tree *pred = tree->left_child;// move pred to line 127 then define it
-	if(tree == NULL) return tree;//base case 
+    struct Tree *pred = tree;
+	if(tree == NULL) return tree;//base case
 	if(tree->val == item){
-		while(pred->right_child)
-			pred = pred->left_child;
-			tree = pred;
-		}
+	 pred = tree->left_child;
+	while(pred->right_child){
+		pred = pred->left_child;
+		tree = pred;
+	}
 	}
 	else{
 		tree = pred;
@@ -185,10 +184,11 @@ int main() {
      int n; //this is the number that has to be saved into the tree
      int min;
      int max;
+     char str1[50];
      int heightTree;
      int predecessor;
      int successor;
-     char str1[50];
+     int numDeletedLeafs;
     struct Tree *root = NULL;
      int res;
      int err;
@@ -209,12 +209,11 @@ int main() {
             }
             else if ((res == 2) &&(strcmp(str1,"DEL")) == 0){
                   if(root == NULL){
-			  root = deletey(root, n);//prints node that doesn't exist in BST
-			  printf("%d\n", n);
-		  }
-		    else if(root != NULL){
-			    root = deletey(root, n);
-			     printf("%d\n", 0);//node is now empty, 
+                    printf("%d\n", 0);
+                    }
+                else if(root != NULL){
+                    numDeletedLeafs = deletey(root, n);
+                    printf("%d\n", numDeletedLeafs);
 		    }
 	    }
            else if (res == 2 &&(strcmp(str1,"SEA")) == 0){
@@ -228,16 +227,17 @@ int main() {
            else if (res == 1 &&(strcmp(str1,"MIN")) == 0){
                 if (minimum(root)== NULL){
                     printf("%d\n", 0);
-                }
+                    }
                 else{
                     min = minimum(root)->val;
                     printf("%d\n", min);
                 }
+           }
            else if (res == 1 &&(strcmp(str1,"MAX")) == 0){
                 max = maximum(root);
                 printf("%d\n", max);
            }
-           else if (res == 2 &&(strcmp(str1,"PRE")) == 0){
+            else if (res == 2 &&(strcmp(str1,"PRE")) == 0){
                 predecessor = pred(root, n)->val;
                 printf("%d\n", predecessor);
            }
@@ -252,4 +252,4 @@ int main() {
     }
     }
      return 0;
-}
+};
